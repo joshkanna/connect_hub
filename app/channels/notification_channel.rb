df39e9e7325e119603fefc423b4c44c9e15dfe94
Joshua <joshuakannappilly10@gmail.com>
2024-04-28 10:11:16 +0100

    got rid of unused channel

--- app/channels/notification_channel.rb
+++
@@ -1,9 +0,0 @@
-class NotificationsChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from current_user
-  end
-
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-end
